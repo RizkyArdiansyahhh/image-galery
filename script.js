c3f6b9ba3999d1679d291eeacd3b63de90451fb1
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-09-13 01:43:11 +0700

    modified index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector(".container");
 const jumbo = document.querySelector(".jumbo");
+const thumb = document.querySelector(".thumb");
 let active = null;
+
+thumb.classList.add("active");
+active = thumb;
 
 container.addEventListener("click", function (e) {
   if (e.target.className == "thumb") {
@@ -9,5 +13,10 @@ container.addEventListener("click", function (e) {
     }
     jumbo.setAttribute("src", e.target.src);
     jumbo.classList.add("fade");
+    e.target.classList.add("active");
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 500);
+    active = e.target;
   }
 });
